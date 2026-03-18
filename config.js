aac3ad5fa5debc1cbbcc84b20ee44b5bd3e2e8b7
heilandoo <61976073+heilandoo@users.noreply.github.com>
2026-03-18 16:17:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/mapbox/dark-v11',
+    style: 'mapbox://styles/mapbox/satellite-v9',
     // leave commented to use Mapbox Standard Style
     accessToken: 'pk.eyJ1IjoiaGVpbGFuZG9vIiwiYSI6ImNtbXVwYXZhZTIxaGwycnIwdG9sYmNxeHMifQ.ypcTVPtTt_UV7j6MNaRYag',
     showMarkers: true,
