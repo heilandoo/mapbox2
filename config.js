24ac3e30ac9b94158c38227d16b722a369e2f5cf
heilandoo <61976073+heilandoo@users.noreply.github.com>
2026-03-31 15:49:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ var config = {
             id: 'pipeline-intro',
             alignment: 'left',
             hidden: false,
-            title: 'H2Med / extracting energy from south to north, powering a never ending growth paradigm',
+            title: 'Extracting energy from south to north, powering a never ending growth paradigm',
             // image: './assets/arcelor-mittal-surf.JPG',
             description: 'In the name of the "green" transition, Europe is building new energy corridors from south to north. The oversized H2Med pipeline promises to transport vast amounts of green hydrogen from iberian peninsula to central-northern Europe. Yet along its route, this vision is challenged by communities who question its impacts, its benefits, and its underlying assumptions. The pipeline’s journey reveals not just a technological project, but a struggle over competing vision of the future.',
             location: {
@@ -53,14 +53,14 @@ var config = {
             id: 'hydrogen-overview',
             alignment: 'right',
             hidden: true,
-            title: 'The H2Med-pipeline',
+            title: 'The H2Med pipeline',
             // image: './assets/washington-dc.jpg',
             // description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
             location: {
-                center: [0.43752, 44.83533],
-                zoom: 3.5,
+                center: [-2.813, 45.916],
+                zoom: 3.9,
                 pitch: 0,
-                bearing: 0,
+                bearing: 0
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -87,15 +87,15 @@ var config = {
         {
             id: 'hydrogen-overviewmap',
             alignment: 'right',
-            hidden: false,
-            title: 'Port of Xixón',
-            image: './assets/IMG_3135.JPG',
-            description: 'Even the very spirit of the hydrogen plan is problematic. Instead of focusing on hydrogen that is produced and used locally, close to where it is needed, the strategy once again prioritizes exports and the construction of large transport infrastructures. These projects include new pipelines, storage facilities, and other large-scale systems that mainly benefit major energy corporations, which are interested above all in expanding their own infrastructure networks. In many cases, these infrastructures may later turn out to be unnecessary or unjustified. Nevertheless, huge amounts of public money are already being invested in them. Ultimately, these costs will likely be passed on to energy consumers in each country. For these reasons, this approach is deeply concerning and difficult to justify.',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
             location: {
-                center: [-5.71677, 43.56260],
-                zoom: 12.52,
-                pitch: 70,
-                bearing: 160.00
+                center: [-2.813, 45.916],
+                zoom: 3.9,
+                pitch: 0,
+                bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -111,6 +111,63 @@ var config = {
             image: './assets/IMG_3135.JPG',
             description: 'Even the very spirit of the hydrogen plan is problematic. Instead of focusing on hydrogen that is produced and used locally, close to where it is needed, the strategy once again prioritizes exports and the construction of large transport infrastructures. These projects include new pipelines, storage facilities, and other large-scale systems that mainly benefit major energy corporations, which are interested above all in expanding their own infrastructure networks.',
             location: {
+              center: [-5.71677, 43.56260],
+              zoom: 12.52,
+              pitch: 70,
+              bearing: 160.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pipeline-impact',
+            alignment: 'right',
+            hidden: false,
+            title: 'title',
+            image: '',
+            description: 'description...',
+            location: {
+                center: [-5.6739, 41.3394],
+                zoom: 15.5,
+                pitch: 61,
+                bearing: 70
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'hydrogen-use',
+            alignment: 'left',
+            hidden: false,
+            title: 'Green growth for northern Europe?',
+            image: '',
+            description: 'hydrogen can be smart to be used for decarbonisation of some specific, necessary energy sectors, but current plans are heavily oversized, hydrogen boom is powered by increasing militarisation and "green" military [Greening the armies](https://www.consilium.europa.eu/media/69640/art_greening_-armies_web.pdf)...',
+            location: {
+                center: [5.67537, 43.56],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'hydrogen-colonialism1',
+            alignment: 'left',
+            hidden: false,
+            title: 'title',
+            image: '',
+            description: 'description...',
+            location: {
                 center: [-5.67537, 43.56],
                 zoom: 4,
                 pitch: 0,
@@ -121,6 +178,101 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }
+        },
+        {
+            id: 'hydrogen-colonialism2',
+            alignment: 'left',
+            hidden: false,
+            title: 'title',
+            image: '',
+            description: 'description...',
+            location: {
+                center: [-5.67537, 43.56],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'transition-fail1',
+            alignment: 'left',
+            hidden: false,
+            title: 'title',
+            image: '',
+            description: 'description...',
+            location: {
+                center: [-5.67537, 43.56],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'transition-fail2',
+            alignment: 'left',
+            hidden: false,
+            title: 'title',
+            image: '',
+            description: 'description...',
+            location: {
+                center: [-5.67537, 43.56],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'resistance-grow',
+            alignment: 'left',
+            hidden: false,
+            title: 'title',
+            image: '',
+            description: 'description...',
+            location: {
+                center: [-5.67537, 43.56],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'alternatives-exist',
+            alignment: 'left',
+            hidden: false,
+            title: 'title',
+            image: '',
+            description: 'description...',
+            location: {
+                center: [-5.67537, 43.56],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
     ]
 };
